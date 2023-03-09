90c668e06e9c851859b202f5118c5ba389f4c8d8
Vusal Alakbarov <vusal11010@gmail.com>
2023-03-09 16:19:11 +0400

    Finished Day 1.

---
+++ Day 1 - Introduction/js/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof fullName);
+console.log(typeof isStudent);
+console.log(typeof internshipGrade);
+console.log(typeof job);
---
+++ Day 1 - Introduction/js/variable.js
@@ -0,0 +1,34 @@
+let fullName = "Vusal Alakbarov";
+let isStudent = true;
+let internshipGrade = undefined;
+let job = null;
+
+let company;
+let field;
+let ceo;
+let sponsor;
+
+let myTeam = "Galatasaray";
+let estDate = 1905;
+let isRecordHolder = true;
+let domesticLeagueTrophies = 22;
+
+/*
+    let firstName = "Vusal";
+    let lastName = "Alakbarov";
+    let maritalStatus = "single";
+    let country = "Azerbaijan";
+    let age = 23;
+*/
+
+let firstName = "Vusal",
+  lastName = "Alakbarov",
+  maritalStatus = "single",
+  country = "Azerbaijan",
+  age = 23;
+
+let myAge = 23;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
